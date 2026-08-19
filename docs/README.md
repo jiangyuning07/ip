2efb6e2cb7b32f96fa26b6ef1297d06b6ab9f7af
Jiang Yuning <137623527+jiangyuning07@users.noreply.github.com>
2026-08-19 14:17:40 +0800

    Update chatbot name and banner

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Alex User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Alex.java
@@ -0,0 +1,10 @@
+public class Alex {
+    public static void main(String[] args) {
+        String banner = "    _    _           \n"
+                + "   / \\  | | _____  __\n"
+                + "  / _ \\ | |/ _ \\ \\/ /\n"
+                + " / ___ \\| |  __/>  < \n"
+                + "/_/   \\_\\_|\\___/_/\\_\\\n";
+        System.out.println(banner);
+    }
+}
