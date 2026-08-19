5c4113eeab5c8d97c000203a803681af6e28e2e9
Jiang Yuning <137623527+jiangyuning07@users.noreply.github.com>
2026-08-19 14:30:44 +0800

    Add chatbot greeting message

--- src/main/java/Alex.java
+++ src/main/java/Alex.java
@@ -1,10 +1,20 @@
 public class Alex {
     public static void main(String[] args) {
+        String divider = "____________________________________________________________";
         String banner = "    _    _           \n"
                 + "   / \\  | | _____  __\n"
                 + "  / _ \\ | |/ _ \\ \\/ /\n"
                 + " / ___ \\| |  __/>  < \n"
                 + "/_/   \\_\\_|\\___/_/\\_\\\n";
-        System.out.println(banner);
+        String greeting = "Hello! I'm Alex.\n"
+                + "What can I do for you?";
+        String farewell = "Bye. Hope to see you again soon!";
+
+        System.out.println(divider);
+        System.out.print(banner);
+        System.out.println(greeting);
+        System.out.println(divider);
+        System.out.println(farewell);
+        System.out.println(divider);
     }
 }
